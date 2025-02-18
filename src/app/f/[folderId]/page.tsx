29b123f6cfe6986aafa99ad9717c669907876656
Luca1905 <contact@lucawang.me>
2025-02-18 20:10:41 +0100

    fixed access control for the file and folders page

--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -1,14 +1,30 @@
 import { QUERIES } from "~/server/db/queries";
 import DriveContents from "./drive-contents";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 export default async function GoogleDriveClone(props: {
   params: Promise<{ folderId: string }>
 }) {
+  const session = await auth();
+  if (!session.userId) {
+    return redirect("/sign-in");
+  }
+
   const params = await props.params;
 
   const parsedFolderId = parseInt(params.folderId);
   if (isNaN(parsedFolderId)) {
     return <div>Invalid folder ID</div>
+  }
+
+  const folder = await QUERIES.getFolderById(parsedFolderId);
+  if (!folder) {
+    return <div>Folder not found</div>
+  }
+
+  if (folder.ownerId !== session.userId) {
+    return <div>You are not the owner of this folder</div>
   }
 
   const [files, folders, parents] = await Promise.all([
